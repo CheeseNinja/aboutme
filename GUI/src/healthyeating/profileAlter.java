788568e0f6626dc6e4e6340047f594066096d3e0
Darren Wang <rardenawng@gmail.com>
2018-02-13 12:31:20 -0500

    Update profileAlter.java

--- GUI/src/healthyeating/profileAlter.java
+++ GUI/src/healthyeating/profileAlter.java
@@ -11,88 +11,89 @@ import java.util.prefs.Preferences;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-//make a pop up to set the area of the pic to put on the label
-//auto resize pic if too big (no need for small pics), background will be black
+public class profileAlter extends JFrame {
+	private JPanel mainPanel, profileBox;
+	private String picLocation = "", sub = "";
+	private JFileChooser fc;
+	private JButton addpic;
+	private JLabel pic;
+	private JFrame editor;
+	private Preferences prefs = Preferences.userRoot().node(this.getClass().getName());
+	private int alteratingInt = 1;
 
-public class profileAlter extends JFrame{
- private JPanel mainPanel, profileBox;
- private String picLocation = "", sub = "";
- private JFileChooser fc;
- private JButton addpic;
- private JLabel pic;
- private JFrame editor;
- private Preferences prefs = Preferences.userRoot().node(this.getClass().getName());
+	public profileAlter() {
+		components();
 
-
- public profileAlter() {
-  components();
-  
-  setSize(800,600);
-  setVisible(true);
-  this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
- }
- 
- public void components() {
-
-  mainPanel = new JPanel(new BorderLayout());
-  profileBox = new JPanel();
-  profileBox.setPreferredSize(new Dimension(250,600));
-   JPanel in = new JPanel();
-   in.setPreferredSize(new Dimension(225,270));
-   in.setBackground(Color.BLACK);
-   pic = new JLabel();
-   pic.setIcon(new ImageIcon(prefs.get(sub, "")));
-   in.add(pic);
-    
-   
-   
-   addpic = new JButton("add image");
-   fc = new JFileChooser();
-   FileNameExtensionFilter filter = new FileNameExtensionFilter(
-        "JPG & GIF Images", "jpg", "gif");
-    fc.setFileFilter(filter);
-   
-   addpic.addActionListener(new ActionListener() {
-
-    @Override
-    public void actionPerformed(ActionEvent arg0) {
-     if(fc.showOpenDialog(addpic) == JFileChooser.APPROVE_OPTION) {
-    	 picLocation = fc.getSelectedFile().getAbsolutePath();
-    	 ImageIcon picture = new ImageIcon(picLocation);
-    	 File fileImage = new File(picLocation);
-    	 prefs.put(sub, System.getProperty("user.home") + "/Desktop/croppedimages/cropped.jpg");
-    	 editorPane ep = new editorPane(picture,fileImage);
-     }
-    }
-    
-   });
-
-  JButton refresh = new JButton("refresh");
-  refresh.addActionListener(new ActionListener() {
-
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		 pic.setIcon(new ImageIcon(System.getProperty("user.home") + "/Desktop/croppedimages/cropped2.jpg"));
+		setSize(800, 600);
+		setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	  
-  });
-  profileBox.add(in);
-  profileBox.add(addpic);
-  profileBox.add(refresh);
-  profileBox.setBackground(Color.BLACK);
-  
-  
-  mainPanel.add("West", profileBox);
-  add(mainPanel);
- }
- public static void hi() {
-	 System.out.println("hi");
- }
-public static void main(String[] args) {
-	SwingUtilities.invokeLater(new Runnable() {
-		public void run() {
-			profileAlter al = new profileAlter();
-	}	 
- });
-}
+
+	public void components() {
+
+		mainPanel = new JPanel(new BorderLayout());
+		profileBox = new JPanel();
+		profileBox.setPreferredSize(new Dimension(250, 600));
+		JPanel in = new JPanel();
+		in.setPreferredSize(new Dimension(225, 270));
+		in.setBackground(Color.BLACK);
+		pic = new JLabel();
+		pic.setIcon(new ImageIcon(prefs.get(sub, "")));
+		in.add(pic);
+
+		addpic = new JButton("add image");
+		fc = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG & GIF Images", "jpg", "gif");
+		fc.setFileFilter(filter);
+
+		addpic.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				if (fc.showOpenDialog(addpic) == JFileChooser.APPROVE_OPTION) {
+					picLocation = fc.getSelectedFile().getAbsolutePath();
+					ImageIcon picture = new ImageIcon(picLocation);
+					File fileImage = new File(picLocation);
+					prefs.put(sub, System.getProperty("user.home") + "/Desktop/croppedimages/cropped.jpg");
+					editorPane ep = new editorPane(picture, fileImage);
+				}
+			}
+
+		});
+
+		JButton refresh = new JButton("refresh");
+		refresh.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				if (alteratingInt % 2 != 0) {
+					pic.setIcon(new ImageIcon(System.getProperty("user.home") + "/Desktop/croppedimages/cropped2.jpg"));
+					alteratingInt++;
+				} else {
+					pic.setIcon(new ImageIcon(System.getProperty("user.home") + "/Desktop/croppedimages/cropped.jpg"));
+					alteratingInt++;
+				}
+			}
+
+		});
+		profileBox.add(in);
+		profileBox.add(addpic);
+		profileBox.add(refresh);
+		profileBox.setBackground(Color.BLACK);
+
+		mainPanel.add("West", profileBox);
+		add(mainPanel);
+	}
+
+	public static void hi() {
+		System.out.println("hi");
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				profileAlter al = new profileAlter();
+			}
+		});
+	}
 }
